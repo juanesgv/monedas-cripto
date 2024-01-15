1b8083bd7d1c6561ccb2bd01c6748026735d98a7
juanesgv <juanesgarviedma23@gmail.com>
2024-01-15 17:15:58 -0500

    implementación de styled componentes y hooks personalizados

---
+++ src/components/Formulario.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import styled from '@emotion/styled'
+import useSelectMonedas from '../hooks/useSelectMonedas'
+
+const InputSubmit =styled.input `
+
+    background-color: #9497FF;
+    border: none;
+    width: 100%;
+    padding: 10px;
+    color: #FFF;
+    font-weight: 700;
+    text-transform: uppercase;
+    font-size: 20px;
+    border-radius:  5px;
+    transition: background-color .3s ease;
+
+    &:hover{
+        background-color: #7A7DFE;
+        cursor: pointer;
+    }
+`
+
+const Formulario = () => {
+  
+    const [SelectMonedas] = useSelectMonedas("Elige tu moneda");
+
+    return (
+        <form>
+            <SelectMonedas/>
+            <InputSubmit type='submit' value='cotizar'/>      
+        </form>
+    )
+}
+
+export default Formulario
---
+++ src/hooks/useSelectMonedas.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import styled from '@emotion/styled'
+
+const Label = styled.label`
+    color: #FFF;
+`
+
+const useSelectMonedas = (label) => {
+  const selectMonedas = ()=>(
+    <>
+        <Label>{label}</Label>
+    </>
+  )
+  return [selectMonedas]
+}
+
+export default useSelectMonedas
