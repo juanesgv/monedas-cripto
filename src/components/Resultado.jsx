1886c9b09cf891e334b05936a811eb91c67d11aa
juanesgv <juanesgarviedma23@gmail.com>
2024-01-16 14:52:15 -0500

    Se hicieron mejoras en los estilos

--- src/components/Resultado.jsx
+++ src/components/Resultado.jsx
@@ -3,31 +3,87 @@ import styled from "@emotion/styled"
 const Resultado = ({ resultado }) => {
 
     const Contenedor = styled.div`
-        color: #fff;
+        background-color: #fff;
+        color: #0D2235;
+        border-radius: 12px;
         font-family: 'Lato', sans-serif;
         display: flex;
+        flex-direction: column;
         align-items: center;
-        gap: 1rem;
+        gap: 10px;
         margin-top: 30px;
+        padding: 16px;
+    `
+
+    const TextoPrecio = styled.p`
+
+        display: flex;
+        flex-direction: column;
+        align-items: flex-start;
+        gap: 4px;
+        align-self: stretch;
+        margin-top: 0px;
+        margin-bottom: 0px;
+
+        font-size: 14px;
+        font-family: 'Lato', sans-serif;
+        font-weight: 400;
+
+        span{
+            font-family: 'Lato', sans-serif;
+            font-size: 24px;
+            font-weight: 700;
+        }
+    `
+    const ContenedorPrecios = styled.div`
+        display: flex;
+        align-items: center;
+        gap: 10px;
+        align-self:stretch;
+    `
+    const ContenedorPreciosInterior = styled.div`
+        display: flex;
+        flex-direction: column;
+        align-items: flex-start;
+        gap: 10px;
+        flex: 1 0 0;
+    `
+    const ContenedorPreciosVariaciones = styled.div`
+        display: flex;
+        justify-content: space-between;
+        align-items: flex-start;
+        align-self: stretch;
     `
 
     const Texto = styled.p`
-        font-size: 18px;
-        span{
-            font-weight: 700;
-        }
-    `
+        display: flex;
+        flex-direction: column;
+        align-items: flex-start;
+        gap: 4px;
+        align-self: stretch;
 
-    const Precio = styled.p`
-        font-size: 24px;
+        font-size: 12px;
+        font-family: 'Lato', sans-serif;
+        font-weight: 400;
+
         span{
+            font-family: 'Lato', sans-serif;
+            font-size: 16px;
             font-weight: 700;
         }
     `
+    const LastUpdate = styled.p`
+        color: rgba(13, 34, 53, 0.60);
+        font-family: Lato;
+        font-size: 10px;
+        font-style: normal;
+        font-weight: 400;
+        line-height: normal;
+    `
+    
     const Imagen = styled.img`
         display: block;
-        width: 120px; 
-
+        width: 80px;
     `
     
 
@@ -35,13 +91,19 @@ const Resultado = ({ resultado }) => {
 
     return (
         <Contenedor>
-            <Imagen src={`https://cryptocompare.com/${IMAGEURL}`} alt="Imagen cripto" />
+            <ContenedorPrecios>
+                <Imagen src={`https://cryptocompare.com/${IMAGEURL}`} alt="Imagen cripto" />
+                <ContenedorPreciosInterior>
+                    <TextoPrecio><span>{PRICE} </span>Precio actual</TextoPrecio>
+                    <ContenedorPreciosVariaciones>
+                        <Texto><span>{HIGHDAY}</span>Precio más alto</Texto>
+                        <Texto><span>{LOWDAY}</span>Precio más bajo</Texto>
+                        <Texto><span>{CHANGEPCT24HOUR}</span>Variación últimas 24 horas</Texto>
+                    </ContenedorPreciosVariaciones>
+                </ContenedorPreciosInterior>
+            </ContenedorPrecios>
             <div>
-                <Precio>El precio es de: <span>{PRICE}</span></Precio>
-                <Texto>Precio más alto del día: <span>{HIGHDAY}</span></Texto>
-                <Texto>Precio más bajo del día: <span>{LOWDAY}</span></Texto>
-                <Texto>Variación de las últimas 24 horas: <span>{CHANGEPCT24HOUR}</span></Texto>
-                <Texto>Última actualización: <span>{LASTUPDATE}</span></Texto>
+                <LastUpdate>Última actualización: {LASTUPDATE}</LastUpdate>
             </div>
         </Contenedor>
     )
